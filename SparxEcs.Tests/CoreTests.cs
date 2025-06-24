7ec2d66b5a7aa10426c2e1524f5add5a6d964b3a
Joseph Heinz <josephheinz28@gmail.com>
2025-06-23 23:19:46 -0400

    started tests

---
+++ SparxEcs.Tests/CoreTests.cs
@@ -0,0 +1,40 @@
+﻿namespace SparxEcs.Tests;
+
+using Xunit;
+using SparxEcs;
+
+public class CoreTests
+{
+    private ECS ecs;
+
+    public CoreTests()
+    {
+        ecs = new ECS();
+    }
+
+    public struct A
+    {
+        public int X;
+    }
+
+    [Fact]
+    public void Test_AddEntity_AssignsUniqueID()
+    {
+        var id1 = ecs.AddEntity();
+        var id2 = ecs.AddEntity();
+        Assert.NotEqual(id1, id2);
+    }
+
+    [Fact]
+    public void Test_AddComponent_ShouldAddComponentToEntity()
+    {
+        var entity = ecs.AddEntity();
+        ecs.RegisterComponent<A>();
+        ecs.Add<A>(entity, new A { X = 10 });
+
+        Assert.True(ecs.HasComponent<A>(entity));
+        var comp = ecs.Get<A>(entity);
+        Assert.NotNull(comp);
+        Assert.Equal(10, comp.X);
+    }
+}
